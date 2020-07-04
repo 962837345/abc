9d9cd070483a3f0d2c3b8abcb1ede41fca08e3b9
962837345 <962837345@qq.com>
2020-07-04 23:12:51 +0800

    delete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="home">
-    <!--    <img alt="Vue logo" src="../assets/logo.png">-->
-    <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <Container></Container>
   </div>
 </template>
@@ -13,7 +11,6 @@
   export default {
     name: 'Home',
     components: {
-      // HelloWorld
       Container
     }
   }
